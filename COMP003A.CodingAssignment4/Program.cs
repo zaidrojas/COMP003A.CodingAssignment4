cf91a640319b71a2042d7c660dcbee10b97dc4cd
zaidrojas <zaidnemesis83@outlook.com>
2025-02-05 17:36:53 -0800

    Update 5:36pm

--- COMP003A.CodingAssignment4/Program.cs
+++ COMP003A.CodingAssignment4/Program.cs
@@ -2,7 +2,7 @@
  Author: Zaid Rojas
  Course: COMP-003A
  Faculty: Jonathan Cruz
- Purpose: Inventory management application with a minimum of 1
+ Purpose: inventory management application with a minimum of 1
 */
 
 using System.Reflection.Emit;
